a6f6378bdb1bc5f910a42c1124750efeb8e0ce96
mf0414 <mf0414>
2017-02-19 19:27:48 -0500

    added error handling to client

--- client.py
+++ client.py
@@ -1,36 +1,68 @@
 import socket
 import sys
 
+#...............................................................#
 packet_size = 1024
-
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Connect the socket to the port where the server is listening
-#Bind Socket to Port
-port = sys.argv[1]
+header_size = 3
+port = int(sys.argv[1])
 ip = sys.argv[2]
 file_name = sys.argv[3]
 out_file = sys.argv[4]
+#...............................................................#
 
-FILE = open(out_file, "wb")
-server_address = (ip,int(port))
-    # Send request
-request = "r" + file_name
-print("Sending Request")
-sent = sock.sendto(request.encode(), server_address)
+def receive_packet(socket, packet, client_address, header):
+   return 0
 
-data,server = sock.recvfrom(packet_size)
-run = 1
-while run == 1:
-   FILE.write(data)
-   data, server = sock.recvfrom(packet_size)
+
+
+
+
+if __name__ = '__main__':
+
+   # Create Socket
    try:
-      if(data.decode() == "halt"):
-         run = 0
-   except UnicodeDecodeError:
-      continue
+      sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+   except Exception:
+      print("Socket Creation Failed. Exiting...")
+      sys.exit()
 
-print("File Done. Closing Socket and File.")
-sock.close()
-FILE.close()
+   #Check Validity of Port
+   if port < 0 or port > 65535:
+      print("Port Number is Invalid. Exiting...")
+      sys.exit()
+
+   #Bind Socket to Port
+   try:
+      server_address = (ip,port)
+      sock.bind(server_address)   
+   except Exception:
+      print("Socket Binding Failed. Exiting...")
+      sys.exit()
+
+   #Open File to Write to
+   try:
+      FILE = open(out_file, "wb")
+   except Exception:
+      print("Save file creation failed. Exiting...")
+      sys.exit()
+   
+   # Send request
+   request = "r" + file_name
+   print("Sending Request")
+   sent = sock.sendto(request.encode(), server_address)
+
+   #Receive File
+   data,server = sock.recvfrom(packet_size)
+   run = 1
+   while run == 1:
+      FILE.write(data)
+      data, server = sock.recvfrom(packet_size)
+      try:
+         if(data.decode() == "halt"):
+            run = 0
+      except UnicodeDecodeError:
+         continue
+
+   print("File Done. Closing Socket and File.")
+   sock.close()
+   FILE.close()
